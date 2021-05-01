e1d2f514e543b53c8083b9c61c45215cb4617d38
walker <2303061691@qq.com>
2021-05-01 18:39:36 +0800

    添加正则表达式帮助类

---
+++ WalkerCommon/WalkerCommon/RegexHelper.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace WalkerCommon
+{
+    public class RegexHelper
+    {
+        /// <summary>
+        /// 获取第一个匹配项
+        /// </summary>
+        /// <param name="str">要匹配的字符串</param>
+        /// <param name="regex">正则表达式</param>
+        /// <returns></returns>
+        public static string GetFirstMatchValue(string str,string regex)
+        {
+            Regex reg = new Regex(regex);
+            //例如我想提取记录中的NAME值
+            Match match = reg.Match(str);
+            string value = match.Groups[1].Value;
+
+            return value;
+        }
+    }
+}
---
+++ WalkerCommon/WalkerCommon/XMLHelper.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Linq;
+
+namespace WalkerCommon
+{
+    public class XMLHelper
+    {
+        public static void CreatXmlTree(string xmlPath)
+        {
+            XElement xElement = new XElement(
+                new XElement("BookStore",
+                    new XElement("Book",
+                        new XElement("Name", "C#入门", new XAttribute("BookName", "C#")),
+                        new XElement("Author", "Martin", new XAttribute("Name", "Martin")),
+                        new XElement("Adress", "上海"),
+                        new XElement("Date", DateTime.Now.ToString("yyyy-MM-dd"))
+                        ),
+                    new XElement("Book",
+                        new XElement("Name", "WCF入门", new XAttribute("BookName", "WCF")),
+                        new XElement("Author", "Mary", new XAttribute("Name", "Mary")),
+                        new XElement("Adress", "北京"),
+                        new XElement("Date", DateTime.Now.ToString("yyyy-MM-dd"))
+                        )
+                        )
+                );
+
+            //需要指定编码格式，否则在读取时会抛：根级别上的数据无效。 第 1 行 位置 1异常
+            XmlWriterSettings settings = new XmlWriterSettings();
+            settings.Encoding = new UTF8Encoding(false);
+            settings.Indent = true;
+            XmlWriter xw = XmlWriter.Create(xmlPath, settings);
+            xElement.Save(xw);
+            //写入文件
+            xw.Flush();
+            xw.Close();
+        }
+    }
+}
