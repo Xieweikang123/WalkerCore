0b34b18a9ab7d6e7c7f484704db1f9d6d55715c4
walker <2303061691@qq.com>
2021-05-04 21:08:35 +0800

    新增 SqlHelper.cs

---
+++ WalkerCommon/WalkerCommon/SqlHelper.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WalkerCommon
+{
+
+    public class SqlHelper
+    {
+        public static string connStr { get; set; }
+
+
+        /// <summary>
+        /// 获取datatable
+        /// </summary>
+        /// <param name="sql"></param>
+        /// <returns></returns>
+        public static DataTable GetTable(string sql)
+        {
+            using (SqlConnection con = new SqlConnection(connStr))
+            {
+                con.Open();
+                using (SqlCommand cmd = new SqlCommand(sql, con))
+                {
+                    cmd.CommandType = CommandType.Text;
+
+                    var table = new DataTable();
+                    var sqlAda = new SqlDataAdapter(cmd);
+                    sqlAda.Fill(table);
+                    return table;
+                }
+            }
+        }
+    }
+}
