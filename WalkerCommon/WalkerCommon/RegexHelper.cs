63ef28e7e6e1d7ff83ff0fdcf179302ba9291ea9
walker <2303061691@qq.com>
2021-05-04 20:47:47 +0800

    正则 参数名修改

--- WalkerCommon/WalkerCommon/RegexHelper.cs
+++ WalkerCommon/WalkerCommon/RegexHelper.cs
@@ -13,11 +13,11 @@ namespace WalkerCommon
         /// 获取第一个匹配项
         /// </summary>
         /// <param name="str">要匹配的字符串</param>
-        /// <param name="regex">正则表达式</param>
+        /// <param name="pattern">正则表达式</param>
         /// <returns></returns>
-        public static string GetFirstMatchValue(string str,string regex)
+        public static string GetFirstMatchValue(string str,string pattern)
         {
-            Regex reg = new Regex(regex);
+            Regex reg = new Regex(pattern);
             //例如我想提取记录中的NAME值
             Match match = reg.Match(str);
             string value = match.Groups[1].Value;
