f62b3cb9336e8b0e166ab389fd767689860272df
walker <2303061691@qq.com>
2021-05-23 01:54:01 +0800

    table to list

---
+++ WalkerCore/GenericityHelper.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WalkerCore
+{
+    public class GenericityHelper
+    {
+
+        /// <summary>
+        /// 将datable转换为list
+        /// </summary>
+        /// <param name="table"></param>
+        /// <param name="columnName">指定列</param>
+        /// <returns></returns>
+        public static List<string> TableToList(DataTable table, string columnName) 
+        {
+            var list = new List<string>();
+
+            foreach (DataRow row in table.Rows)
+            {
+                list.Add(row[columnName].ToString());
+            }
+
+            return list;
+        }
+    }
+}
