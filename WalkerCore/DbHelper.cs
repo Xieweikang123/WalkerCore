55e496fbed6b310cbbee735042997bdded163f3f
walker <2303061691@qq.com>
2021-05-23 18:23:52 +0800

    dbhelper  获取列名与值

--- WalkerCore/DbHelper.cs
+++ WalkerCore/DbHelper.cs
@@ -9,14 +9,14 @@ namespace WalkerCore
     public class DbHelper
     {
 
-        public  string connStr { get; set; }
+        public string connStr { get; set; }
 
         /// <summary>
         /// 获取datatable
         /// </summary>
         /// <param name="sql"></param>
         /// <returns></returns>
-        public  DataTable GetTable(string sql)
+        public DataTable GetTable(string sql, SqlParameter[] sqlParameters = null)
         {
             using (SqlConnection con = new SqlConnection(connStr))
             {
@@ -24,12 +24,52 @@ namespace WalkerCore
                 using (SqlCommand cmd = new SqlCommand(sql, con))
                 {
                     cmd.CommandType = CommandType.Text;
-
+                    if (sqlParameters != null)
+                    {
+                        cmd.Parameters.AddRange(sqlParameters);
+                    }
                     var table = new DataTable();
                     var sqlAda = new SqlDataAdapter(cmd);
                     sqlAda.Fill(table);
                     return table;
                 }
+            }
+        }
+        public List<Dictionary<string, string>> GetRowList(string sql,out List<string> columnNames , SqlParameter[] sqlParameters = null)
+        {
+            var rowList = new List<Dictionary<string,string>>();
+            using (SqlConnection con = new SqlConnection(connStr))
+            {
+                con.Open();
+                using (SqlCommand cmd = new SqlCommand(sql, con))
+                {
+                    cmd.CommandType = CommandType.Text;
+                    if (sqlParameters != null)
+                    {
+                        cmd.Parameters.AddRange(sqlParameters);
+                    }
+                    var dr = cmd.ExecuteReader();
+                    var fieldCount = dr.FieldCount;
+
+                    var colSchemas = dr.GetColumnSchema();
+                    columnNames = new List<string>();
+
+                    foreach (var col in colSchemas)
+                    {
+                        columnNames.Add(col.ColumnName);
+                    }
+                    while (dr.Read())
+                    {
+                        var listTemp = new Dictionary<string,string>();
+                        for (var i = 0; i < fieldCount; i++)
+                        {
+                            listTemp.Add(columnNames[i], dr[i].ToString());
+                        }
+                        rowList.Add(listTemp);
+                    }
+                    dr.Close();
+                }
+                return rowList;
             }
         }
     }
